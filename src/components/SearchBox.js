755a8c81f34135c2da0c4ab5c22728ac69ab0f22
bikramshankhar <56664732+bikramshankhar@users.noreply.github.com>
2020-06-25 12:27:41 -0500

    SearchBox.js

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,27 +3,27 @@ import React from "react"
 function SearchBox(props) {
     return (
         <div class="jumbotron">
-        <div class="container">
-            <h2 className="text-center">Associate Directory</h2>
-            <form>
-                <div className="input-group mb-3">
-              
-                    <input
-                        onChange={props.handleInputChange}
-                        value={props.search}
-                        name="search"
-                        type="text"
-                        className="form-control"
-                        placeholder="Search for an Employee"
-                    />
-                <div onClick={props.handleFormSubmit} className="input-group-append">
-                <span className="input-group-text">search</span>
+            <div class="container">
+                <h2 className="text-center">Associate Directory</h2>
+                <form>
+                    <div className="input-group mb-3">
+                
+                        <input
+                            onChange={props.handleInputChange}
+                            value={props.search}
+                            name="search"
+                            type="text"
+                            className="form-control"
+                            placeholder="Search for an Employee"
+                        />
+                    <div onClick={props.handleFormSubmit} className="input-group-append">
+                    <span className="input-group-text">search</span>
 
-                </div>
-                </div>
-            </form>
-      </div>
-      </div>
+                    </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
   }
   
