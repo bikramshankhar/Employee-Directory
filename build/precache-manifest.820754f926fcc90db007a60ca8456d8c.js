bc5c4aac391b60f5e0324c5af19da1466f88a41e
bikramshankhar <56664732+bikramshankhar@users.noreply.github.com>
2020-06-27 11:28:06 -0500

    build package

---
+++ build/precache-manifest.820754f926fcc90db007a60ca8456d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a30bff618f8fa5008711faf8886012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "755efe4b15d160d28ba8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c5906de4b9aea3ae0c7d",
+    "url": "/static/js/2.58366293.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58366293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755efe4b15d160d28ba8",
+    "url": "/static/js/main.89c4a8c0.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
